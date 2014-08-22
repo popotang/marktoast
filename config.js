2ce2027b4640d45a47156b5aace19345e3225abd
梓骞 <tangshuang.ts@alibaba-inc.com>
2014-08-22 09:37:37 +0800

    update config and head

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var rc = require('rc')
   , defaultConfig = {
-      title: 'Online Markdown Editor - Dillinger, the Last Markdown Editor ever.'
-    , description:  'Dillinger is an online cloud based HTML5 filled Markdown Editor. '+
-                    'Sync with Dropbox, Github and Google Drive. 100% Open Source!'
+      title: '马克吐司'
+    , description:  'markdown语法书写印象笔记'
+
     , googleWebmasterMeta: 'DAyGOgtsg8rJpq9VVktKzDkQ1UhXm1FYl8SD47hPkjA'
   }
   ;
